0a0a96393577fab598aea569e4251079bb418837
João Vitor Moraes <jvmb@cesar.school>
2025-04-08 20:07:11 -0300

    Merge branch 'master' of https://github.com/CelioJr12/AtividadeContinuadaPOO.git

--- ac20251/src/br/edu/cs/poo/ac/seguro/entidades/Veiculo.java
+++ ac20251/src/br/edu/cs/poo/ac/seguro/entidades/Veiculo.java
@@ -3,11 +3,13 @@ import lombok.Setter;
 import lombok.Getter;
 import lombok.AllArgsConstructor;
 
+import java.io.Serializable;
 @Setter 
 @Getter
 @AllArgsConstructor
 
-public class Veiculo {
+public class Veiculo implements Serializable{
+    private static final long serialVersionUID = 1L;
 	private String placa;
 	private int ano;
 	private SeguradoEmpresa proprietarioEmpresa;
