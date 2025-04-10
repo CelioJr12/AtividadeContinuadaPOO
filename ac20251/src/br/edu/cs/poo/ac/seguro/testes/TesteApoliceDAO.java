913bbadb8c11456c5013c7ac9b267f37e5d7c506
CelioJr12 <celiojunio878@gmail.com>
2025-04-10 11:54:10 -0300

    Começo de testeApoliceDAO

--- ac20251/src/br/edu/cs/poo/ac/seguro/testes/TesteApoliceDAO.java
+++ ac20251/src/br/edu/cs/poo/ac/seguro/testes/TesteApoliceDAO.java
@@ -4,8 +4,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import br.edu.cs.poo.ac.seguro.daos.ApoliceDAO;
+import br.edu.cs.poo.ac.seguro.daos.SeguradoEmpresaDAO;
 import br.edu.cs.poo.ac.seguro.entidades.Apolice;
 
-public class TesteApoliceDAO {
-
+public class TesteApoliceDAO extends TesteDAO{
+	private ApoliceDAO dao = new ApoliceDAO();
+	protected Class getClasse() {
+		return Apolice.class;
+	}
+	
+	
 }
