f1b4d1b14af99b7370b7170025a586eb076e9f8f
João Vitor Moraes <jvmb@cesar.school>
2025-05-05 19:00:55 -0300

    comeco inclusao

--- ac20251/src/br/edu/cs/poo/ac/seguro/mediators/DadosVeiculo.java
+++ ac20251/src/br/edu/cs/poo/ac/seguro/mediators/DadosVeiculo.java
@@ -1,22 +1,20 @@
-package br.edu.cs.poo.ac.seguro.mediators;
+package br.edu.cs.poo.ac.seguro.entidades;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
-import lombok.AllArgsConstructor;
+import br.edu.cs.poo.ac.seguro.entidades.CategoriaVeiculo;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
-@AllArgsConstructor
+@Setter
 public class DadosVeiculo {
-	private String cpfOuCnpj;
-	private String placa;
-	private int ano;
-	private BigDecimal valorMaximoSegurado;
-	private int codigoCategoria;
-	void setCodigoCategoria(int codigoCategoria) {
-		this.codigoCategoria = codigoCategoria;
-	}
-	void setAno(int ano) {
-		this.ano = ano;
-	}
+    private String placa;
+    private CategoriaVeiculo categoria;
+    private int anoFabricacao;
+    private BigDecimal valor;
+    private String cpfOuCnpj;
+    private boolean indicadorLocadora;
+    private LocalDate inicioVigencia;
 }
